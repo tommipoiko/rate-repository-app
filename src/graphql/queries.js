8ccf43ec64251bb878507e9830b28ba9b1925106
tommipoiko <tommi.poiko@gmail.com>
2023-03-04 19:18:49 +0200

    10.11 done

---
+++ src/graphql/queries.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client'
+
+export const ALL_REPOSITORIES = gql`
+  query {
+    repositories {
+      totalCount
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        endCursor
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          name
+          ownerName
+          createdAt
+          fullName
+          reviewCount
+          ratingAverage
+          forksCount
+          stargazersCount
+          description
+          language
+          ownerAvatarUrl
+        }
+      }
+    }
+  }
+`
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,14 @@
+import { useQuery } from '@apollo/client';
+import { ALL_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, loading, error, refetch } = useQuery(ALL_REPOSITORIES, {fetchPolicy: 'cache-and-network'});
+
+  if (data) {
+    return { repositories: data.repositories, loading, error, refetch };
+  } else {
+    return { repositories: null, loading, error, refetch };
+  }
+};
+
+export default useRepositories;
